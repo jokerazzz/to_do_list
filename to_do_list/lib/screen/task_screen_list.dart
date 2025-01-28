0483bef89e2ffd74063f6091af29ed75c287ef75
Azamat Ozokeev <youiever.ao@gmail.com>
2025-01-29 05:09:11 +0600

    created basic functional

---
+++ to_do_list/lib/screen/task_screen_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskListScreen extends StatefulWidget {
+  const TaskListScreen({super.key});
+
+  @override
+  State<TaskListScreen> createState() => _TaskScreenListState();
+}
+
+class _TaskScreenListState extends State<TaskListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
